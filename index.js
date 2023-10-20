5a6807978c8ff2ab059f8ab51fbe636c8da75f92
LMKgaming <leminh.khoa2907@gmail.com>
2023-10-20 21:19:24 +0700

    New Snow Icon

--- index.js
+++ index.js
@@ -25,6 +25,7 @@ const colorData = [
     "#c718a7",
 ];
 const blurData = ["4px", "5px", "6px", "7px", "8px"];
+const blurDataSnow = ["1px", "2px", "3px", "4px"];
 
 const motherText = [
     "Ngày 20/10, con không biết nói gì ngoài gửi đến mẹ lời chúc mạnh khỏe, trẻ mãi không già, luôn là người phụ nữ xinh đẹp nhất trong lòng con. Mãi yêu mẹ❤️❤️❤️",
@@ -39,45 +40,56 @@ const friendText = [
 
 const girlFriendText = [
     "Trên thiên đường có 10 thiên thần: 5 thiên thần đang chơi đùa, 4 thiên thần đang nói chuyện và 1 thiên thần đang đọc tin nhắn này. Chúc thiên thần của anh vui vẻ, hạnh phúc và ý nghĩa trong ngày 20/10 nhé!❤️❤️❤️",
-    "Trên Trái Đất có 7.000.000.000 người. Và anh không hiểu vì sao anh chỉ nhắn tin chúc mừng 20/10 cho mỗi mình em. Có lẽ vì 6.999.999.999 người còn lại không thể thay thế được em.❤️❤️❤️"
-]
+    "Trên Trái Đất có 7.000.000.000 người. Và anh không hiểu vì sao anh chỉ nhắn tin chúc mừng 20/10 cho mỗi mình em. Có lẽ vì 6.999.999.999 người còn lại không thể thay thế được em.❤️❤️❤️",
+];
 
 const root = document.querySelector("#root");
-const initRoseBackground = (count) => {
+const initSnowBackground = (count, includeSnowIcon = false) => {
     for (let i = 0; i < count; i++) {
-        let chooseColor = Math.floor(Math.random() * (colorData.length));
-        let chooseBlur = Math.floor(Math.random() * (blurData.length));
+        let chooseColor = Math.floor(Math.random() * colorData.length);
+        let chooseBlur = Math.floor(Math.random() * (includeSnowIcon ? blurDataSnow.length : blurData.length));
         let randomLeft = Math.floor(Math.random() * width - 15);
         let randomTop = Math.floor(Math.random() * height - 15);
         if (randomTop + (50 / 100) * height > height) {
             let overVh = Math.ceil(((randomTop + (50 / 100) * height - height) / height) * 100);
             randomTop = randomTop - (height / 100) * Math.floor(Math.random() * overVh + overVh);
         }
-        let widthElement = Math.floor(Math.random() * 5) + 8;
+        let widthElement = Math.floor(Math.random() * 5) + includeSnowIcon ? 15 : 5;
         let timeAnimation = Math.floor(Math.random() * 8) + 5;
         const span = document.createElement("span");
-        span.style.background = colorData[chooseColor];
-        span.style.filter = `blur(${blurData[chooseBlur]})`;
+        if (includeSnowIcon) {
+            span.innerHTML = `<i class="fa-solid fa-snowflake"></i>`;
+            span.style.color = colorData[chooseColor];
+            span.style.fontSize = widthElement + "px";
+            span.style.filter = `blur(${blurDataSnow[chooseBlur]})`;
+            span.children[0].style.animation = `rotate ${(timeAnimation / 2).toFixed(
+                1
+            )}s linear infinite`;
+        } else {
+            span.style.background = colorData[chooseColor];
+            span.style.width = widthElement + "px";
+            span.style.height = widthElement + "px";
+            span.style.filter = `blur(${blurData[chooseBlur]})`;
+        }
         span.style.top = randomTop + "px";
         span.style.left = randomLeft + "px";
-        span.style.width = widthElement + "px";
-        span.style.height = widthElement + "px";
         span.style.animation = `move ${timeAnimation}s ease-out infinite`;
         span.classList.add("snow-item");
         root.appendChild(span);
     }
 };
-initRoseBackground(200);
+initSnowBackground(200);
+initSnowBackground(200, true)
 
 const envelope = document.querySelector(".envelope-btn");
 const envelopeText = document.querySelector(".envelope-text");
 const secondScreen = document.querySelector(".second-screen");
 const noteScreen = document.querySelector(".note-screen");
-const noteLine = document.querySelector('.note-line')
+const noteLine = document.querySelector(".note-line");
 
-;((chooseText = friendText) => {
-    let randomText = Math.floor(Math.random() * (chooseText.length))
-    noteLine.innerText = chooseText[randomText]
+((chooseText = friendText) => {
+    let randomText = Math.floor(Math.random() * chooseText.length);
+    noteLine.innerText = chooseText[randomText];
 })(friendText);
 
 const handleClickEnvelope = () => {
